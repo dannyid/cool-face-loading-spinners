db30c0bcfe46303e983a40d04f433102fc2f3e7b
dannyid <danielshekhtman@gmail.com>
2015-06-21 14:14:49 -0400

    Add ES6 polyfill to use spread without browser error

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,13 @@
 import cool from 'cool-ascii-faces';
 import colorSchemes from './constants/colorschemes.js';
+import 'babel/polyfill';
 
 const faceDiv = document.getElementsByClassName('face-spinner')[0];
 
 const colorScheme = colorSchemes[Math.floor(Math.random() * colorSchemes.length)];
 
-const face = cool().split('').map(function(x, i) {
+const face = [...cool()].map((x, i) => {
   return `<span style="color: ${colorScheme[i % colorScheme.length]};">${x}</span>`;
 }).join('');
-  
+
 faceDiv.innerHTML = face;
