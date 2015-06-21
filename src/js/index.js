5a534b7cf23df6b6d384f728ceb9b59d17ee5bad
dannyid <danielshekhtman@gmail.com>
2015-06-21 04:07:24 -0400

    Add bouncing motion and various color schemes

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,12 @@
 import cool from 'cool-ascii-faces';
+import colorSchemes from './constants/colorschemes.js';
 
-document.getElementsByClassName('face-spinner')[0].innerHTML = cool();
+const faceDiv = document.getElementsByClassName('face-spinner')[0];
+
+const colorScheme = colorSchemes[Math.floor(Math.random() * colorSchemes.length)];
+
+const face = cool().split('').map(function(x, i) {
+  return `<span style="color: ${colorScheme[i % colorScheme.length]};">${x}</span>`;
+}).join('');
+  
+faceDiv.innerHTML = face;
